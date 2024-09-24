7795a23c2a53289c2e5844003c217dca27aa27d6
Tilak Patel <tilakny@gmail.com>
2024-09-24 18:48:10 -0400

    final

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function TOC() {
   return (
     <ul>
+      <h1>Tilak Patel</h1>
       <li><Link to="/Labs">Labs</Link></li>
       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
