44387a037868f317077f4950a7b58f27d3cec970
Tilak Patel <tilakny@gmail.com>
2024-09-24 22:38:20 -0400

    fixes

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,14 +1,9 @@
-import { Link } from "react-router-dom"
-export default function KanbasNavigation() {
+import { Link } from "react-router-dom";
+export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a> <br/>
-      <Link to="/Kanbas/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kanbas/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
+      <Link to={`/Kanbas/Account/Signin`}  > Signin  </Link> <br/>
+      <Link to={`/Kanbas/Account/Signup`}  > Signup  </Link> <br/>
+      <Link to={`/Kanbas/Account/Profile`} > Profile </Link> <br/>
     </div>
-  )
-}
+);}
